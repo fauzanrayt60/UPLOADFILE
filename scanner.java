be7a49c4bfd2a9396c33c81846354763c0d5c119
Fauzan Ray.T <55981039+fauzanraytarigan@users.noreply.github.com>
2019-10-12 21:24:35 +0700

    Update scanner.java

--- scanner.java
+++ scanner.java
@@ -8,49 +8,24 @@ public class beratbadan {
     
     public static void main(String[] args) {
         
-        int bb;
-        double tb;
-        String nama,status;
-	Scanner sc = new Scanner(System.in);
-	nama = sc.next();
-	tb = sc.nextDouble();
-	bb = sc.nextInt();
-		//Masukkan
-		//	Nama				  : contoh Fauzan Ray
-		//	Tinggi badan (Dalam ukuran meter) : contoh 1.70
-		//	Berat badan			  : contoh 46
-		double hasil = (bb/(tb*tb));
-			
-		if (hasil<=18.5){
-			System.out.println(nama+"kamu kurus, ayo naikkan berat badanmu :) ");
-		}
-		else if (hasil==18.6){
-			System.out.println(nama+",berat badanmu normal, jaga pola hidup sehat agar tetap normal :D ");
-		}
-		else if (hasil<=19){
-			System.out.println(nama+",berat badanmu normal, jaga pola hidup sehat agar tetap normal :D ");
-		}
-		else if (hasil<=20){
-			System.out.println(nama+",berat badanmu normal, jaga pola hidup sehat agar tetap normal :D ");
-		}
-		else if (hasil<=21){
-			System.out.println(nama+",berat badanmu normal, jaga pola hidup sehat agar tetap normal :D ");
-		}
-		else if (hasil<=22){
-			System.out.println(nama+",berat badanmu normal, jaga pola hidup sehat agar tetap normal :D ");
-		}
-		else if (hasil<=23){
-			System.out.println(nama+",berat badanmu normal, jaga pola hidup sehat agar tetap normal :D ");
-		}
-		else if (hasil<=24){
-			System.out.println(nama+",berat badanmu normal, jaga pola hidup sehat agar tetap normal :D ");
-		}
-		else if (hasil<=24.9){
-			System.out.println(nama+",berat badanmu normal, jaga pola hidup sehat agar tetap normal :D ");
-		}
-		else if (hasil>=25){
-			System.out.println(nama+" kamu Gemuk, ayo kurus kan badan mu :D ");
-		}
+       Scanner qq = new Scanner(System.in);
+        String nama;
+        System.out.print("Masukkan nama :");
+        nama = qq.nextLine();
+        float bb,tb,hasil;
+        System.out.print("Masukkan Berat badan :");
+        bb = qq.nextFloat();
+        System.out.print("Masukkan Tinggi badan :");
+        tb = qq.nextFloat();
+        hasil = (bb/((tb/100)*(tb/100)));
+        System.out.println(hasil);
+         if  (hasil<=18.5)
+        System.out.println(nama+" kamu kurus, ayo naikkan berat badan mu");
+    else if(hasil>=18.6 && hasil<25.0)
+        System.out.println(nama+" kamu normal, tetap jaga pola hidup sehat");
+    else if(hasil>=25.1 && hasil<29)
+        System.out.println(nama+" kamu gemuk, ayo olah raga dan jalani pola hidup sehat");
+        
        
     }
     
